a35e14cb55fb4ec947a4eb984b4a5288daeffa27
Amanze <amanze@Amanzes-Mac.local>
2015-11-25 00:07:58 +0100

    Added AJAX comment to the question segment of the project

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,9 +2,28 @@ class CommentsController < ApplicationController
   def index
   end
 
-  def create
+  def new
+  	@comment = Comment.new
+  	@question = Question.find(params[:question_id])
   end
 
-  def show
+  def create
+  	@comment = Comment.new(comment_param)
+  	@comment.user_id = params[:user_id]
+  	@comment.question_id = params[:question_id]
+    
+  	respond_to do |format|
+  		if @comment.save
+  			format.html { }
+  			format.js
+  	    end
+  	end
   end
+
+  private
+    def comment_param
+      params.require(:comment).permit(:content, 
+      								  :user_id, 
+      								  :question_id)
+    end
 end
