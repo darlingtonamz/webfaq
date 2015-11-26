5297748714ef975db37ab8d8137e580d0f4b7f37
MUSTAPHA <mustaphaalade@gmail.com>
2015-11-26 13:04:22 +0000

    Finished rating

--- app/models/link.rb
+++ app/models/link.rb
@@ -2,8 +2,8 @@ class Rating < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :question
 	acts_as_votable
-
-
-  
+	def score
+      self.get_upvotes.size - self.get_downvotes.size
+    end
 end
-end
+
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,11 @@
 class Question < ActiveRecord::Base
 	belongs_to :faq
 	has_many :ratings
+	acts_as_votable
 	has_many :comments, dependent: :delete_all
+
+	def score
+      self.get_upvotes.size - self.get_downvotes.size
+    end
+
 end
