1c024a4ecaedacecd09a4f058aa5763816a71852
Edwin Tsatsu <paynegreen@yahoo.com>
2015-11-27 10:31:34 +0000

    CSS fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,19 @@
-    Rails.application.routes.draw do
-
-      devise_for :users
-      devise_scope :user do
-      	# get "/" => "devise/sessions#new"
-        
-      resources :users do
-      	resources :faqs do
-      		resources :questions do
-            member do
-              put "like", to: "questions#upvote"
-              put "dislike", to: "questions#downvote"
-            end  
-           end
-        end
-      end
-      resources :comments
-      resources :ratings
-      
-      
-      root to: "users#show"
-
-
+Rails.application.routes.draw do
+  # TODO match '/faqs/:token' => 'questions#faq_show'
+  devise_for :users
+  devise_scope :user do
+  resources :users do
+  	resources :faqs do
+  		resources :questions do
+        member do
+          put "like", to: "questions#upvote"
+          put "dislike", to: "questions#downvote"
+        end  
+       end
     end
   end
+  resources :comments
+  resources :ratings
+  root to: "users#show"
+end
+end
