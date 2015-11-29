a4c8c6ee6dccc37c036d5a0a43c067ac615664f6
Edwin Tsatsu <paynegreen@yahoo.com>
2015-11-29 17:15:36 +0000

    Adds 'website_domain' field to the registration/edit view of devise to enable input data

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,9 +1,10 @@
 class Question < ActiveRecord::Base
-	belongs_to :faq
-	has_many :ratings, dependent: :delete_all
+	belongs_to 		:faq
+	has_many 		:ratings, dependent: :delete_all
 	acts_as_votable
-	has_many :comments, dependent: :delete_all
-	validates :content, :answer, presence: true
+	has_many 		:comments, dependent: :delete_all
+	validates 		:content,
+					:answer, presence: true
 
 	def score
       self.get_upvotes.size - self.get_downvotes.size
