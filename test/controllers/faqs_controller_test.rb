b84d590821df674c15cb8ba9e714447c7bb42831
Edwin Tsatsu <paynegreen@yahoo.com>
2015-12-05 15:28:26 +0000

    Writng tests for the various controllers

--- test/controllers/faqs_controller_test.rb
+++ test/controllers/faqs_controller_test.rb
@@ -1,9 +1,21 @@
 require 'test_helper'
 
 class FaqsControllerTest < ActionController::TestCase
+	
+	def setup
+		@faq = Faq.new(title: "Google is the answer", user_id: 1)
+	end
+
   test "should show an faq" do
-    get :show
-    assert_template 'faqs/show'
-    assert_not_nil assigns(:faq)
+  	user1 = users(:user1)
+
+    get :index, {user_id: user1.id}
+    assert_template 'faqs/index'
   end
+
+  # test "create an faq" do
+  # 	user1 = users(:user1)
+
+  # 	get :create
+  # end
 end
--- test/models/faq_test.rb
+++ test/models/faq_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class FaqTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	def setup
+		@faq = Faq.new(title:"Google is the way to go", user_id: 1)
+	end
+  
+  test "create faqs" do
+  	assert @faq.save
+  end
 end
